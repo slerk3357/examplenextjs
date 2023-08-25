6fed81c5600110d9bcc2e0209b66dd0efa663245
slerk3357 <slerke399@hotmail.com>
2023-08-25 21:26:06 +0900

    to SSG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
@@ -16,26 +15,24 @@ export default function Home() {
                         rel="noopener noreferrer"
                     >
                         By{' '}
-                        <Image
+                        <img
                             src="/vercel.svg"
                             alt="Vercel Logo"
                             className={styles.vercelLogo}
                             width={100}
                             height={24}
-                            priority
                         />
                     </a>
                 </div>
             </div>
 
             <div className={styles.center}>
-                <Image
+                <img
                     className={styles.logo}
                     src="/next.svg"
                     alt="Next.js Logo"
                     width={180}
                     height={37}
-                    priority
                 />
             </div>
 
